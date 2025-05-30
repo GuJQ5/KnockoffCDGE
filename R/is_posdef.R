4a0f48dc2f695ee9224aa7abf4ef5bc0037eeb34
Jiaqi Gu <jiaqigu@sh02-ln03.stanford.edu>
2025-05-30 11:28:16 -0700

    Update: Fix issue of SPD_no_julia

--- R/is_posdef.R
+++ R/is_posdef.R
@@ -10,13 +10,15 @@
 #'
 is_posdef = function(A) {
   A<-(A+t(A))/2
-  if(nrow(A)<=2)
-  {
+  # if(nrow(A)<=2)
+  # {
     lambda_min<-min(eigen(A,only.values = TRUE)$values)
-  }
-  else
-  {
-    lambda_min<-eigs(A, 1, which = "SR")$values
-  }
+  # }
+  # else
+  # {
+    # AA<--A
+    # diag(AA)<-diag(AA)+nrow(AA)
+    # lambda_min<-nrow(AA)-eigs(AA, 1, which = "LM")$values
+  # }
   return (lambda_min>1e-6)
 }
